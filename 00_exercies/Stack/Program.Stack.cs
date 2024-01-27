ea37f071e0d702bab0214c41f362201d082c4e23
Wes Benvindo <wal2.designer@gmail.com>
2024-01-27 18:12:33 +0000

    Adding and returning last element

--- 00_exercies/Stack/Program.Stack.cs
+++ 00_exercies/Stack/Program.Stack.cs
@@ -1,10 +1,27 @@
-﻿namespace Stack
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
 {
     internal partial class Program
     {
         public class Stack
         {
-            
+            private List<object> _stackObjects = new List<object> { };
+
+            public void Push(object obj)
+            {
+                _stackObjects.Add(obj);
+            }
+
+            public object Pop()
+            {
+                return (object) _stackObjects.Last();
+            }
+
+            public void Clear()
+            {
+            }
         }
     }
 }
