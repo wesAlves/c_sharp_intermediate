95d88f4dafc7ea3b94e4f9ff4700ae437239390a
Wes Benvindo <wal2.designer@gmail.com>
2024-01-29 20:18:54 +0000

    just do it

---
+++ 00_exercies/EditoMTZ/EditoMTZ/Program.cs
@@ -0,0 +1,13 @@
+﻿//TODO:
+    //Upload a file, in format .docx or .xml
+    //The file must have attributes for each block in xml structure
+    //Create a entry in database, file, building block, user processing
+
+    using EditoMTZ.ebook;
+
+    var ebook01 = new HTMLFormat();
+    ebook01.Create();
+    
+
+    Console.WriteLine(ebook01.id);
+    // Console.WriteLine(ebook01.name);
---
+++ 00_exercies/EditoMTZ/EditoMTZ/ebook/Ebook.cs
@@ -0,0 +1,19 @@
+﻿namespace EditoMTZ.ebook;
+
+public abstract class Ebook
+{
+    public string name { get; set; }
+    public string id { get; set; }
+
+
+
+    public abstract void Create();
+}
+
+public class HTMLFormat : Ebook
+{
+    public override void Create()
+    {
+        Console.WriteLine("Creating HTML data file");
+    }
+}
